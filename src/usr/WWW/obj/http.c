b2b01aed495ba4d663ad0e8893ad6630317dbec4
Felix A. Croes <felix@dworkin.nl>
2012-11-07 00:15:24 +0100

    Move basic HTTP functionality into separate object.

--- src/usr/WWW/obj/http.c
+++ src/usr/WWW/obj/http.c
@@ -1,8 +1,7 @@
 # include <kernel/kernel.h>
 # include <http.h>
 
-inherit HTTP_USER;
-inherit "~/lib/http_protocol";
+inherit "~/lib/user";
 
 private inherit "/lib/util/string";
 private inherit "/lib/util/time";
@@ -10,73 +9,12 @@ private inherit "/lib/util/time";
 
 # define CHUNK_SIZE	65535
 
-int response_code;
-
 static void create(int clone)
 {
     if (clone) {
 	::create();
 	call_out("disconnect", 300);
     }
-}
-
-static int query_code() { return response_code; }
-
-private object
-response(int code, string str,
-	 varargs string type, int length, int modtime, string body)
-{
-    response_code = code;
-    if (!body) {
-	body = "";
-    }
-    return new_object("/data/strbuffer",
-		      http_response(code, str, type, length, modtime) + body);
-}
-
-private object bad_request()
-{
-    string str;
-
-    str = "<HTML><HEAD><TITLE>Bad Request</TITLE></HEAD>" +
-	  "<BODY><H1>" + HTTP_BAD_REQUEST + " Bad Request</H1>" +
-	  "This server could not understand your query." +
-	  "</BODY></HTML>";
-    return response(HTTP_BAD_REQUEST, nil, "text/html", strlen(str), 0, str);
-}
-
-private object not_implemented()
-{
-    string str;
-
-    str = "<HTML><HEAD><TITLE>Not Implemented</TITLE></HEAD>" +
-	  "<BODY><H1>" + HTTP_NOT_IMPLEMENTED + " Not Implemented</H1>" +
-	  "Unimplemented server function." +
-	  "</BODY></HTML>";
-    return response(HTTP_NOT_IMPLEMENTED, nil, "text/html", strlen(str), 0,
-		    str);
-}
-
-private object not_found()
-{
-    string str;
-
-    str = "<HTML><HEAD><TITLE>Not Found</TITLE></HEAD>" +
-	  "<BODY><H1>" + HTTP_NOT_FOUND + " Not Found</H1>" +
-	  "This server could not find the path you specified." +
-	  "</BODY></HTML>";
-    return response(HTTP_NOT_FOUND, nil, "text/html", strlen(str), 0, str);
-}
-
-private object unauthorized(string realm)
-{
-    string str;
-
-    str = "<HTML><HEAD><TITLE>Unauthorized</TITLE></HEAD>" +
-	  "<BODY><H1>" + HTTP_UNAUTHORIZED + " Unauthorized</H1>" +
-	  "You must supply a user name and password to use this method." +
-	  "</BODY></HTML>";
-    return response(HTTP_UNAUTHORIZED, realm, "text/html", strlen(str), 0, str);
 }
 
 private string file_type(string file)
