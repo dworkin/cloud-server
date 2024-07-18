cad4a06bb72cb1350d6f35c8030ff67737138d61
Felix A. Croes <felix@dworkin.nl>
2024-07-18 16:00:09 +0200

    Add WebSocket closing status codes.

--- src/usr/HTTP/api/include/HttpConnection.h
+++ src/usr/HTTP/api/include/HttpConnection.h
@@ -8,7 +8,7 @@
 # define HTTP1_TLS_SERVER	"/usr/HTTP/api/obj/tls_server1"
 # define HTTP1_TLS_CLIENT	"/usr/HTTP/api/obj/tls_client1"
 
-# define WEBSOCK_OPCODE		0x0f	/* opcode */
+# define WEBSOCK_OPCODE		0x0f		/* opcode */
 # define WEBSOCK_CONTINUATION	0x00
 # define WEBSOCK_TEXT		0x01
 # define WEBSOCK_BINARY		0x02
@@ -16,8 +16,21 @@
 # define WEBSOCK_PING		0x09
 # define WEBSOCK_PONG		0x0a
 
-# define WEBSOCK_FLAGS		0xf0	/* flags */
+# define WEBSOCK_FLAGS		0xf0		/* flags */
 # define WEBSOCK_FIN		0x80
 # define WEBSOCK_RSV1		0x40
 # define WEBSOCK_RSV2		0x20
 # define WEBSOCK_RSV3		0x10
+
+# define WEBSOCK_STATUS_1000	"\x03\xe8"	/* normal */
+# define WEBSOCK_STATUS_1001	"\x03\xe9"	/* going away */
+# define WEBSOCK_STATUS_1002	"\x03\xea"	/* protocol error */
+# define WEBSOCK_STATUS_1003	"\x03\xeb"	/* invalid input */
+# define WEBSOCK_STATUS_1005	"\x03\xed"	/* missing status code */
+# define WEBSOCK_STATUS_1006	"\x03\xee"	/* closed abnormally */
+# define WEBSOCK_STATUS_1007	"\x03\xef"	/* format error */
+# define WEBSOCK_STATUS_1008	"\x03\xf0"	/* policy violation */
+# define WEBSOCK_STATUS_1009	"\x03\xf1"	/* message too big */
+# define WEBSOCK_STATUS_1010	"\x03\xf2"	/* missing extension */
+# define WEBSOCK_STATUS_1011	"\x03\xf3"	/* general failure */
+# define WEBSOCK_STATUS_1015	"\x03\xf7"	/* TLS handshake failure */
