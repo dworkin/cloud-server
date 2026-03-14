9994bc3ce1bca28e99e35b0da63b3d93cc66a111
Felix A. Croes <felix@dworkin.nl>
2026-03-14 10:31:22 +0100

    Add float2string().

--- src/lib/util/ascii.c
+++ src/lib/util/ascii.c
@@ -63,3 +63,50 @@ static string stringify(string str)
 
     return str;
 }
+
+/*
+ * float to string, with full accuracy
+ */
+static string float2string(float flt)
+{
+    string str, str2, str3, exp;
+    float flt2;
+
+    str = (string) flt;
+    flt2 = (float) str;
+    sscanf(str, "%se%s", str, exp);
+
+    if (flt != flt2) {
+	str2 = str;
+	str2[strlen(str2) - 1]--;
+	str3 = (string) fabs(flt - (float) (str2 + exp));
+	if (fabs(flt) > fabs(flt2)) {
+	    /* string representation was rounded down */
+	    str2 = str3[1 ..];
+	    sscanf(str2, ".%s", str2);
+	    sscanf(str2, "%se", str2);
+	} else {
+	    /* string representation was rounded up */
+	    str = str2;
+	    str2 = str3;
+	    sscanf(str2, "%se", str2);
+	    if (sscanf(str2, "%s.%s", str2, str3) != 0) {
+		str2 += str3;
+	    }
+	}
+
+	/* extend mantissa */
+	if (sscanf(str, "%*s.") == 0) {
+	    str += ".";
+	}
+	str += str2;
+    }
+
+    if (exp) {
+	str += "e" + exp;
+    } else if (sscanf(str, "%*s.") == 0) {
+	str += ".0";
+    }
+
+    return str;
+}
